c116152eaf881b33f48136f93c651b1c679f86f1
Daniil <davydovdanya18@gmail.com>
2020-07-10 15:11:01 +0300

    switched from webflux to webmvc

--- src/main/java/ru/uip/controller/AccountController.java
+++ src/main/java/ru/uip/controller/AccountController.java
@@ -1,6 +1,7 @@
 package ru.uip.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import ru.uip.model.CreateJsonAccount;
